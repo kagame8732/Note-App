cd692d9040998e297be240a933656b707fef2719
kagame8732 <kagame8732@gmail.com>
2021-11-25 17:50:35 +0200

    mode commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = ({ handleDarkMode }) => {
+    return <div className="header">
+        <h1>Notebook</h1>
+        <button
+            onClick={() => handleDarkMode((previousdarkMode) => !previousdarkMode)}
+            className="save">Mode</button>
+    </div >
+}
+
+export default Header;
